3fa0c049489fb91ac3d0b9c18ac9be8964e69e2b
nalutm <nayara.luiza21@gmail.com>
2022-06-22 22:44:57 -0300

    :ok_hand: style: Refactored code

--- responsividade/assets/script.js
+++ responsividade/assets/script.js
@@ -3,20 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const $addNewAppointment = document.querySelector('[data-js="add-new-appointment"]');
   const $newAppointments = document.querySelector('[data-js="new-appointments"]');
 
-  const events = ['click', 'touchstart'];
+  $addNewAppointment.addEventListener('click', () => {
+    $newAppointments.classList.toggle('new-appointments--active');
+    if ($newAppointments.classList.contains('new-appointments--active')) {
+      $newAppointments.classList.remove('new-appointments--inactive');
+      disableScroll();
+    } else {
+      document.body.classList.remove("stop-scrolling");
+    }
+  });
 
-  events.forEach(e => {
-    $addNewAppointment.addEventListener(e, () => {
-      $newAppointments.classList.toggle('new-appointments--active');
-      if ($newAppointments.classList.contains('new-appointments--active')) {
-        $newAppointments.classList.remove('new-appointments--inactive');
-        disableScroll();
-      } else {
-        document.body.classList.remove("stop-scrolling");
-      }
-    });
-  })
-  
   function disableScroll() {
     document.body.classList.add("stop-scrolling");
   }
